e034f591bde456796c5e1da63270bf64acb72ba4
scars321 <48868493+scars321@users.noreply.github.com>
2021-03-17 16:53:09 +0000

    Add files via upload

---
+++ scraper.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+header ={'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/601.3.9 (KHTML, like Gecko) Version/9.0.2 Safari/601.3.9'}
+url='https://news.ycombinator.com/'
+response=requests.get(url,headers=header)
+
+#print(response.content)
+
+soup=BeautifulSoup(response.content, 'lxml')
+
+for item in soup.find_all('tr'):
+    if item.select('.storylink'):
+        print(item.select('.storylink')[0].get_text())
+        print(item.select('.storylink')[0]['href'])
+        print("________________")
+
+    if item.select('.score'):
+        print(item.select('.score')[0].get_text())
+        sorted(item.select('.score'))
+
+       
+    
